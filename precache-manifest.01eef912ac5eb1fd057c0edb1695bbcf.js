86537491cad1cca1062b02d6623636e04f377ff3
Luis David López Valdes <davidvaldes90@hotmail.com>
2019-10-10 23:29:47 -0500

    Buil update

---
+++ precache-manifest.01eef912ac5eb1fd057c0edb1695bbcf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6abe4a9e3ef7df9c3f960d2c3018884",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2caa3a2b5cebe1351563",
+    "url": "./static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "cfce07ee3e8f1494b02b",
+    "url": "./static/css/main.33237812.chunk.css"
+  },
+  {
+    "revision": "2caa3a2b5cebe1351563",
+    "url": "./static/js/2.8898ebc5.chunk.js"
+  },
+  {
+    "revision": "cfce07ee3e8f1494b02b",
+    "url": "./static/js/main.31baeb3f.chunk.js"
+  },
+  {
+    "revision": "d6464e56d800cba7aa84",
+    "url": "./static/js/runtime-main.3349092c.js"
+  },
+  {
+    "revision": "77965140d323111cb1f62bd081849496",
+    "url": "./static/media/Logo-ITS.77965140.png"
+  },
+  {
+    "revision": "798ea9aa28fef8dc7cb07fe01c0edd43",
+    "url": "./static/media/Logo-SEP.798ea9aa.png"
+  },
+  {
+    "revision": "9fff0f3d30b2e7bcc42e160db493af7b",
+    "url": "./static/media/Logo-TecNM.9fff0f3d.png"
+  },
+  {
+    "revision": "e0dfa3639c1aaa79978fa11c815bfad6",
+    "url": "./static/media/alex.e0dfa363.png"
+  },
+  {
+    "revision": "1e336fbbc1bb39d3a887dabdf0e47055",
+    "url": "./static/media/alexis.1e336fbb.png"
+  },
+  {
+    "revision": "c727c1ea496b5abc767c625458576b82",
+    "url": "./static/media/andres.c727c1ea.png"
+  },
+  {
+    "revision": "e45a6343a56bef073221c595d0c115d2",
+    "url": "./static/media/david.e45a6343.png"
+  },
+  {
+    "revision": "09a191de06564733c8c97ab8f25ac1bd",
+    "url": "./static/media/davidlopez.09a191de.png"
+  },
+  {
+    "revision": "e1cece6e37e9f26139b8dc361ce8471a",
+    "url": "./static/media/gabriela.e1cece6e.png"
+  },
+  {
+    "revision": "c543f402e91fb14a97829f5a1a38efcf",
+    "url": "./static/media/guillermo.c543f402.png"
+  },
+  {
+    "revision": "6f7beb50fac1e96285c75ce754e45dcd",
+    "url": "./static/media/janice.6f7beb50.png"
+  },
+  {
+    "revision": "a49a12024cc75f3202dc5d804ff499ad",
+    "url": "./static/media/jorge.a49a1202.png"
+  },
+  {
+    "revision": "198bc52cff0c0bbed150eeab6927a817",
+    "url": "./static/media/logo-cit.198bc52c.png"
+  },
+  {
+    "revision": "2fbff92690a273db880e435d1b160ee0",
+    "url": "./static/media/mariana.2fbff926.png"
+  },
+  {
+    "revision": "691d789ce71bef49d5387443182c7105",
+    "url": "./static/media/nico.691d789c.png"
+  },
+  {
+    "revision": "7ecb25092e299bd4fcd6875340f0893f",
+    "url": "./static/media/pinedo.7ecb2509.png"
+  },
+  {
+    "revision": "2832dbbcfb3500aa57eb768ffabbc9cc",
+    "url": "./static/media/salma.2832dbbc.png"
+  },
+  {
+    "revision": "dbcc7afcf1d0725a3f4651fc44a1007f",
+    "url": "./static/media/sergio.dbcc7afc.png"
+  }
+]);
